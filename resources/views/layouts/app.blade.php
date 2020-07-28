ec23660b1fb02586678b44ddccedfed3e6471fca
vagrant <vagrant@homestead>
2020-07-28 15:10:44 +0000

    add error section to app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -86,6 +86,15 @@
 
 <div class="content">
     <div style="margin: 10px;">
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
         @yield('content')
     </div>
 </div>
