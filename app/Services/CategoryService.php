baa26b1c964941766203877d6608272bdf9ce92c
vagrant <vagrant@homestead>
2020-07-28 15:06:59 +0000

    add CategoryService to Project

---
+++ app/Services/CategoryService.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Services;
+
+use App\Repositories\CategoryRepository;
+use Illuminate\Http\Request;
+
+class CategoryService
+{
+    protected $category_repository;
+    public function __construct(CategoryRepository $category_repository)
+    {
+        $this->category_repository = $category_repository;
+    }
+
+    public function save(Request $request)
+    {
+        return $this->category_repository->save($request);
+    }
+
+    public function getAll()
+    {
+        return $this->category_repository->getAll();
+    }
+}
